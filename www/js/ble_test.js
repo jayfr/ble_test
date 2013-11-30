b7d2f9bb3b041ac8aa5c1a9f1ad0b3568c11bb3a
Jay Francis <jay@reactivetechnologies.com>
2013-11-30 13:18:49 -0500

    testing

--- www/js/ble_test.js
+++ www/js/ble_test.js
@@ -1,4 +1,10 @@
 var bluetoothSerial = cordova.require('bluetoothSerial');
+var cmd = 0x00;
+
+var Left = 0x08;
+var Right = 0x04;
+var Forward = 0x02;
+var Backward = 0x01;
 
 function reset() {
 }
@@ -12,6 +18,39 @@ function disconnect() {
   bluetoothSerial.disconnect(function (){ alert('Success');}, function() { alert('Fail');});
 }
 
-function foo() {
-  bluetoothSerial.write('Foo');
+function send() {
+  bluetoothSerial.write(cmd);
 }
+
+function forward(s) {
+  if (s)
+    cmd |= Forward;
+  else
+    cmd &= ~Forward;
+  send();
+}
+
+function backward(s) {
+  if (s)
+    cmd |= Backward;
+  else
+    cmd &= ~Backward;
+  send();
+}
+
+function left(s) {
+  if (s)
+    cmd |= left;
+  else
+    cmd &= ~left;
+  send();
+}
+
+function right(s) {
+  if (s)
+    cmd |= Right;
+  else
+    cmd &= ~Right;
+  send();
+}
+
