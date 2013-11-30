07711dc9747f0271237620f7f6dc42114d25229d
Jay Francis <jay@reactivetechnologies.com>
2013-11-30 11:37:19 -0500

    testing

--- www/js/ble_test.js
+++ www/js/ble_test.js
@@ -1,6 +1,6 @@
 var bluetoothSerial = cordova.require('bluetoothSerial');
 
 function reset() {
-  bluetoothSerial.connect('', function (){ alert('Success');}, function() { alert('Fail');});
+  bluetoothSerial.connect('2CC79854-57BE-0849-4A44-C51107685679', function (){ alert('Success');}, function() { alert('Fail');});
 }
 
